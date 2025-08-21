3eede8110370686a244822dd8bf5a079992640c9
Leafarex <158201502+Leafarex@users.noreply.github.com>
2025-08-20 17:04:24 -0700

    Update main.cpp

--- tarea trends/main.cpp
+++ tarea trends/main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <chrono>
 #include <functional>
+#include <numeric>
 
 class RandomSumTask {
 public:
@@ -49,23 +50,23 @@ private:
 };
 
 int main() {
-    constexpr int kThreadsCount = 10;
-    constexpr int kNumbersPerThread = 100;
-    constexpr int kRandomMinInclusive = 1;
-    constexpr int kRandomMaxInclusive = 1000;
+    constexpr int THREAD_COUNT = 10;
+    constexpr int NUMBERS_PER_THREAD = 100;
+    constexpr int RANDOM_MIN_INCLUSIVE = 1;
+    constexpr int RANDOM_MAX_INCLUSIVE = 1000;
 
     // Tareas (una por thread)
     std::vector<RandomSumTask> workerTasks;
-    workerTasks.reserve(kThreadsCount);
-    for (int threadIndex = 0; threadIndex < kThreadsCount; ++threadIndex) {
-        workerTasks.emplace_back(kNumbersPerThread, kRandomMinInclusive, kRandomMaxInclusive);
+    workerTasks.reserve(THREAD_COUNT);
+    for (int threadIndex = 0; threadIndex < THREAD_COUNT; ++threadIndex) {
+        workerTasks.emplace_back(NUMBERS_PER_THREAD, RANDOM_MIN_INCLUSIVE, RANDOM_MAX_INCLUSIVE);
     }
 
     std::vector<std::thread> workerThreads;
-    workerThreads.reserve(kThreadsCount);
-    std::vector<std::thread::id> workerThreadIds(kThreadsCount);
+    workerThreads.reserve(THREAD_COUNT);
+    std::vector<std::thread::id> workerThreadIds(THREAD_COUNT);
 
-    for (int threadIndex = 0; threadIndex < kThreadsCount; ++threadIndex) {
+    for (int threadIndex = 0; threadIndex < THREAD_COUNT; ++threadIndex) {
         workerThreads.emplace_back([threadIndex, &workerTasks, &workerThreadIds]() {
             workerTasks[threadIndex].run();
             workerThreadIds[threadIndex] = std::this_thread::get_id();
@@ -74,7 +75,7 @@ int main() {
     for (auto& threadObj : workerThreads) threadObj.join();
 
     // Imprimir resultados
-    for (int threadIndex = 0; threadIndex < kThreadsCount; ++threadIndex) {
+    for (int threadIndex = 0; threadIndex < THREAD_COUNT; ++threadIndex) {
         std::cout << "Thread " << threadIndex
                   << " (id " << workerThreadIds[threadIndex] << ") -> suma = "
                   << workerTasks[threadIndex].getSum() << ", numeros aleatorios = (";
@@ -86,14 +87,12 @@ int main() {
         std::cout << ")\n";
     }
 
-    // Thread con mayor suma
-    int indexOfMaxSum = static_cast<int>(std::distance(
-        workerTasks.begin(),
-        std::max_element(workerTasks.begin(), workerTasks.end(),
-                         [](const RandomSumTask& left, const RandomSumTask& right) {
-                             return left.getSum() < right.getSum();
-                         })
-    ));
+    // Thread con mayor suma usando std::sort y tomando el primero
+    std::vector<int> indices(THREAD_COUNT);
+    std::iota(indices.begin(), indices.end(), 0);
+    std::sort(indices.begin(), indices.end(),
+              [&](int a, int b) { return workerTasks[a].getSum() > workerTasks[b].getSum(); });
+    int indexOfMaxSum = indices.front();
 
     std::cout << "\nEl thread con puntuacion mas alta es el " << indexOfMaxSum
               << " (id " << workerThreadIds[indexOfMaxSum] << ") con suma = "
@@ -101,3 +100,4 @@ int main() {
 
     return 0;
 }
+
